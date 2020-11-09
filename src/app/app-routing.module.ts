ffbc79f7c3341df0b4285a79c08f4814804528b3
= <=>
2020-11-09 17:39:38 +0100

    canvis

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { CustomerDetailComponent } from './customer/customer-detail/customer-detail.component';
 import { CustomerListComponent } from './customer/customer-list/customer-list.component';
-/* import { NotfoundComponent } from './../../../../bcncodes/projects/gest-comercial/src/app/commons/notfound/notfound.component'; */
+  /* import { NotfoundComponent } from './../../../../bcncodes/projects/gest-comercial/src/app/commons/notfound/notfound.component'; */
 import { HomeComponent } from './commons/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
--- src/app/storing/storing-routing.module.ts
+++ src/app/storing/storing-routing.module.ts
@@ -1,12 +1,12 @@
 import { OutgoingComponent } from './outgoing/outgoing.component';
 import { IncomingComponent } from './incoming/incoming.component';
-import { StoringListComponent } from './../../../../../bcncodes/projects/gest-comercial/src/app/storing/storing-list/storing-list.component';
+/* import { StoringListComponent } from '../../../src/app/storing/list/list.component'; */
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', component: StoringListComponent},
-  {path: 'incoming', component:IncomingComponent},
+  /* {path: '', component: StoringListComponent}, */
+  {path: 'incoming', component: IncomingComponent},
   {path: 'outgoing', component: OutgoingComponent}
 ];
 
